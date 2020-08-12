48cc8ed352b73dbcfceea016f430c681dcf9d0c3
Дахновский Дмитрий Александрович <dmitriy.dakhnovskiy@south.rt.ru>
2020-08-13 01:58:54 +0300

    implement get_asteroid

--- src/api/v1/views/asteroids.py
+++ src/api/v1/views/asteroids.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+
+from src.api.v1.views.serializers import Asteroids
+from src.asteroids_data.asteroids_data import get_asteroid_data_from_storage_by_name
 
 asteroids_views = APIRouter()
 
@@ -8,6 +11,7 @@ async def upload_asteroids():
     return {"message": "asteroid added"}
 
 
-@asteroids_views.get('/asteroids', status_code=201)
-async def get_asteroid():
-    return {"message": "asteroid added"}
+@asteroids_views.get('/asteroids', response_model=Asteroids, status_code=200)
+async def get_asteroid(name: str = Query(..., min_length=1)):
+    asteroid_data = await get_asteroid_data_from_storage_by_name(name)
+    return asteroid_data
---
+++ src/api/v1/views/serializers/asteroids.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class OrbitClass(BaseModel):
+    orbit_class_type: str
+    orbit_class_description: str
+    orbit_class_range: str
+
+
+class OrbitalData(BaseModel):
+    orbit_id: str
+    orbit_class: OrbitClass
+
+
+class Asteroids(BaseModel):
+    id: str
+    neo_reference_id: str
+    name: str
+    designation: str
+    nasa_jpl_url: str
+    absolute_magnitude_h: float
+
+    orbital_data: OrbitalData
+
+    images_list: List[str] = []
