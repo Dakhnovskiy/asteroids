a570d9f51687aded9e8a8ec0626dd7a30c5853d0
Дахновский Дмитрий Александрович <dmitriy.dakhnovskiy@south.rt.ru>
2020-08-16 19:48:50 +0300

    +422, 404 errors

--- src/api/v1/views/asteroids.py
+++ src/api/v1/views/asteroids.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, HTTPException
 from starlette.requests import Request
 
 from src.api.v1.views.serializers import Asteroids
@@ -11,6 +11,8 @@ asteroids_views = APIRouter()
 @asteroids_views.get('/asteroids', response_model=Asteroids, status_code=200)
 async def get_asteroid(request: Request, name: str = Query(..., min_length=1)):
     asteroid_data = await get_asteroid_data_from_storage_by_name(name)
+    if not asteroid_data:
+        raise HTTPException(status_code=404, detail=f'asteroid name {name} not found')
     images_ids = await get_images_ids_by_asteroid_name(name)
     asteroid_data['images_links'] = [get_image_download_url(str(request.base_url), image_id) for image_id in images_ids]
     return asteroid_data
--- src/api/v1/views/images.py
+++ src/api/v1/views/images.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import List
 
-from fastapi import APIRouter, UploadFile, File, Form
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from fastapi.responses import FileResponse
 
 from src.api.v1.views.serializers import Images
@@ -18,26 +18,24 @@ async def upload_image(
         asteroids_names: List[str] = Form(...),
 ):
     exists_asteroids = await exists_asteroids_data_by_names(asteroids_names)
-    if exists_asteroids:
-        image = await save_image(await file.read(), file.filename, image_datetime, asteroids_names)
+    if not exists_asteroids:
+        raise HTTPException(status_code=422, detail=f'Incorrect asteroids names')
 
-        return image
-    else:
-        # TODO: validation error
-        return {"message": "assssssss"}
+    image = await save_image(await file.read(), file.filename, image_datetime, asteroids_names)
+    return image
 
 
 @images_views.get('/images/{image_id}', response_model=Images, status_code=200)
 async def get_image(image_id: int):
-    # TODO: not found error
-
     image = await get_image_data_by_id(image_id)
+    if not image:
+        raise HTTPException(status_code=404, detail=f'image id {image_id} not found')
     return image
 
 
 @images_views.get('/images/{image_id}/file')
 async def get_image_file(image_id: int):
-    # TODO: not found error
-
     image = await get_image_data_by_id(image_id)
+    if not image:
+        raise HTTPException(status_code=404, detail=f'image id {image_id} not found')
     return FileResponse(image['path'])
